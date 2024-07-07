31b9341d0b6b78e970ef431ca873d4cc6b0100c1
eray sona <eraysonaonline@gmail.com>
2024-07-07 22:31:51 +0300

    a

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 import "./globals.css"
 
 import { Stage,Circle,Layer,Rect,Text,Group } from "react-konva";
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 import TalkCard from '../components/TalkCard'
 
 export default function Home() {
@@ -12,11 +12,19 @@ export default function Home() {
     let [cardEmoji,setCardEmoji] = useState("");
     let [cardTitle,setCardTitle] = useState("");
 
-    function handleCardItems(event,element, index){
+    function handleClickEmoji(event,element, index){
+        document.getElementById("TalkCard").classList.remove('hidden');
         setCardEmoji(element.emoji);
         setCardTitle(element.name);
     }
 
+    useEffect(()=>{
+        document.addEventListener("keydown",(event)=>{
+            if(event.key === "Escape" || event.key === "Esc"){
+                document.getElementById("TalkCard").classList.add('hidden');
+            }
+        })
+    })
 
     function getRandomInt(min, max) {
         min = Math.ceil(min);
@@ -29,42 +37,45 @@ export default function Home() {
         // 1024 600
         let eachTileX = 28;
         let eachTileY = 20;    
-        
         // emoji and name
         let places=[
             {emoji:"🎢",name:"park"},
             {emoji:"🛖",name:"home"},
             {emoji:"🏫",name:"school"},
             {emoji:"💒",name:"church"},
-            {emoji:"🕌",name:"mousqe"},
+            {emoji:"🕌",name:"mosque"},
             {emoji:"🏥",name:"hospital"},
-            {emoji:"🏯",name:"china"}
-        ]
-        
-        //coord
-
+            {emoji:"🏯",name:"china"},
+            {emoji:"🪐",name:"NASA"},
+            {emoji:"🚢",name:"ship"},
+            {emoji:"🏪",name:"fastfood"},
+            {emoji:"⛺",name:"camping"},
+            {emoji:"❄️",name:"snowball"},
+            {emoji:"🌊",name:"surf"},
+            {emoji:"⛱️",name:"beach"}
+        ]        
         for(let i = 0;i<5;i++){
             let generatedX = getRandomInt(0,16) * eachTileX;
             let generatedY = getRandomInt(0,12) * eachTileY;
             let generatedEmojiNameObj = places[getRandomInt(0,places.length-1)];
-
             let generatedPlace = {
                 emoji:generatedEmojiNameObj.emoji,
                 name:generatedEmojiNameObj.name,
                 xCord:generatedX,
                 yCord:generatedY,
             }
-
             setMapObjs((t) => [...t,generatedPlace])
-            console.log(mapObjs)
         }
 
     }
 
     return (
         <main className='w-full h-screen bg-gray-950 flex flex-row-reverse items-center justify-around'>
-            <TalkCard emoji={cardEmoji} title={cardTitle}></TalkCard>
+            <div className="hidden" id="TalkCard">
+                <TalkCard emoji={cardEmoji} title={cardTitle}></TalkCard>
+            </div>
             
+
             <div className="w-2/3 h-screen bg-gray-950 flex flex-col items-center justify-center">
                 <Stage draggable className="bg-slate-900" width={1024} height={600}>
                     <Layer>
@@ -89,7 +100,7 @@ export default function Home() {
                                     }}
                                     fontSize={80}
                                     fill="white"
-                                    onClick={((event) => handleCardItems(event,element,index))}
+                                    onClick={((event) => handleClickEmoji(event,element,index))}
                                     >
                                 </Text>
 
--- src/components/TalkCard.jsx
+++ src/components/TalkCard.jsx
@@ -1,37 +1,81 @@
 import React from 'react'
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 import '../app/globals.css'
 
 
 const TalkCard = (props) => {
 
+  let [chatHist,setChatHist] = useState(["hi eray how are you?"]);
+
+  function handleChatHistory(input){
+    setChatHist(t => [input,...t])
+  }
+
+  
 
   return (
-      <div className='bg-slate-800 card-size rounded-3xl flex flex-col'>
-          <div className="flex m-3 h-20 flex-row  w-full items-center justify-around">
+      <div className='bg-gray-950 card-size rounded-l-3xl flex flex-col overflow-y-scroll scrollbar overflow-x-hidden border border-white'>
+          <div className="border-2 p-12 rounded-tl-3xl flex h-20 flex-row  w-full items-center justify-around">
             <h2 className='text-7xl select-none relative'>{props.emoji}</h2>
             <h2 className='text-4xl select-none font-bold'>{props.title}</h2>
           </div>
-          <MyBubble></MyBubble>
+          <div id="InputBubble">
+            <MyInputBubble callback={handleChatHistory}></MyInputBubble>
+          </div>
+          {chatHist.map((element,index)=>{
+            if(index%2===0){
+              return <MyBubble key={index} text={element}></MyBubble>
+            }else{
+              return <AIBubble key={index} text={element}></AIBubble>
+            }
+          })}
+          <p className='text-center'>(esc)</p>
     </div>
 
   )
 }
 
-export const MyBubble = () => {
+
+export const AIBubble = (props) => {
+  return (
+    <div className='w-2/3 bg-blue-950 h-auto font-bold text-white p-4 m-5 rounded-2xl border border-white'>
+      <h2 className='pb-2'>Gerry:</h2>
+      <p className=''>{props.text}</p>
+    </div>
+  )
+}
+
+
+
+export const MyBubble = (props) => {
+  return (
+    <div className="w-full flex justify-end">
+      <div className='w-2/3 bg-zinc-950 h-auto font-bold text-white p-4 m-5 rounded-2xl border border-white'>
+        <h2 className="pb-2">me:</h2>
+        <p className=''>{props.text}</p>
+      </div>
+    </div>
+  )
+}
+
+export const MyInputBubble = (props) => {
   let [myInput,setMyInput] = useState("");
-  
+
+  function handleClickAnswer(){
+    props.callback(myInput)
+    setMyInput("");
+  }
+
   function handleMyInput(e){
     setMyInput(e.target.value);
   }
 
   return (
-    <div className="flex justify-end m-3">
-      <textarea type="text" value={myInput} onChange={(e)=>handleMyInput(e)} className='text-black font-bold h-16 w-2/3 p-2 rounded-2xl '></textarea>
+    <div className="flex flex-col items-end justify-end m-5">
+      <textarea type="text" value={myInput} onChange={(e)=>handleMyInput(e)} className='text-white bg-gray-700 rounded-2xl font-bold h-16 w-2/3 p-2  rounded-1xl '></textarea>
+      <button onClick={handleClickAnswer} className='bg-gray-800 w-32'>Answer</button>
     </div>
   )
 }
-
-
 
 export default TalkCard;
